c8008c1a5aa078414e1bc587a0d5fbde96bc445a
suleiman01x <suleiman01x@users.noreply.github.com>
2020-10-10 16:19:18 +0900

    Add MIDI write option

--- commands/chord.js
+++ commands/chord.js
@@ -1,6 +1,28 @@
 const usageText = require('./usage.json').chord;
 const teoria = require('teoria');
 const {capArray} = require('../lib/musicFormatting');
+const MidiWriter = require('midi-writer-js');
+const fs = require('fs');
+
+function makeMidiTrack(teoriaNotes) {
+  //inits track
+  var track = new MidiWriter.Track();
+  track.addEvent(new MidiWriter.NoteEvent({
+    pitch: teoriaNotes[0].scientific(),
+    duration: '1'
+  }));
+
+  for (var note in teoriaNotes) { //adds each note from 1
+    var tempTrack = new MidiWriter.Track();
+    tempTrack.addEvent(new MidiWriter.NoteEvent({
+      pitch: teoriaNotes[note].scientific().toString(),
+      duration: '1'
+    }))
+
+    track.mergeTrack(tempTrack);
+  }
+  return track;
+}
 
 module.exports = {
   name: 'chord',
@@ -15,10 +37,33 @@ module.exports = {
     } catch (error) {
       return message.reply(`${args[0]}はコードと認識されません`);
     }
-    
-    const chordName = chord.name;
-    const chordNotes = chord.simple()
 
-    message.channel.send(`${chordName}の音: ${capArray(chordNotes)}`);
+    // declare parsed chord
+    const chordName = chord.name;
+    const chordNotes = chord.notes()
+    const chordNoteNames = chord.simple()
+
+    if (args.includes('-midi')) { //write Midi
+      var chordTrack = makeMidiTrack(chordNotes);
+      var writer = new MidiWriter.Writer(chordTrack);
+      writer.saveMIDI(chordName);
+      var hasMidi = true;
+    }
+
+    console.log(`FILE NAME: ${chordName}`);
+    message.channel.send(`${chordName}の音: ${capArray(chordNoteNames)}`);
+    //send Midi file
+    if (hasMidi) {message.channel.send({
+      files: [{
+        attachment: `./${chordName}.mid`,
+        name: `${chordName}.mid`
+        }]
+      })
+      .then(() => {
+        fs.unlink(`${chordName}.mid`, (err) => {
+          if(err) console.log(err)
+        });
+      });
+    }
   }
-};
+}
