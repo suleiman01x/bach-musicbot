065b8a7506f2706eb127f92f147f6478fe98bcd5
suleiman01x <suleiman01x@users.noreply.github.com>
2020-10-10 17:22:19 +0900

    Add MIDI write option

--- commands/scale.js
+++ commands/scale.js
@@ -2,10 +2,26 @@ const teoria = require('teoria');
 const {capArray, capitalize} = require('../lib/musicFormatting');
 const Fuse = require('fuse.js');
 const usageText = require('./usage.json').scale;
+const MidiWriter = require('midi-writer-js');
+const fs = require('fs');
 
 
 const scales = ['major', 'minor', 'ionian', 'dorian', 'phrygian', 'lydian', 'mixolydian', 'aeolian', 'locrian', 'pentatonic', 'chromatic', 'blues', 'harmonicminor', 'melodicminor', 'wholetone']
 const fuseScales = new Fuse(scales, {includeScore: true});
+
+function makeMidiTrack(teoriaNotes) {
+  var track = new MidiWriter.Track();
+  for (var note in teoriaNotes){ //converts teoria note to scientific. then adds note event to track
+    var midiNote = teoriaNotes[note].scientific();
+    console.log(midiNote);
+    track.addEvent(new MidiWriter.NoteEvent({
+      pitch: midiNote,
+      duration: '8'
+      }))
+  }
+  return track;
+}
+
 
 module.exports = {
   name: 'scale',
@@ -26,6 +42,7 @@ module.exports = {
       note = note.substring(0, note.length - 1)
     }
 
+    //mode option
     if (args.includes('-mode')) {
       const commandMode = fuseScales.search(args[args.indexOf('-mode') + 1])[0].item;
       if (commandMode === 'pentatonic') { //super jank. changes major/minor pentatonic depending on the notes ending in 'm'
@@ -35,14 +52,38 @@ module.exports = {
       }
     }
 
-    var scaleKeyNote = teoria.note('c')
+    //defines 
+    var scaleKeyNote = teoria.note('c');
     try { //makes note and catches errors
       scaleKeyNote = teoria.note(note);
     } catch (error) {
       console.log(error);
       return message.reply(`${note}は根音になれません`);
     }
-    const scaleNotes = scaleKeyNote.scale(mode).simple();
-    message.channel.send(`${note.toUpperCase()} ${capitalize(mode)}: ${capArray(scaleNotes)}`);
+    const scaleNotes = scaleKeyNote.scale(mode);
+    const scaleName = `${note.toUpperCase()}_${capitalize(mode)}`;
+
+    //midi option
+    if (args.includes('-midi')) {
+      var scaleMidi = makeMidiTrack(scaleNotes.notes());
+      var writer = new MidiWriter.Writer(scaleMidi)
+      writer.saveMIDI(scaleName);
+      var hasMidi = true;
+    }
+
+
+    message.channel.send(`${scaleName}: ${capArray(scaleNotes.simple())}`);
+    if (hasMidi) {message.channel.send({
+      files: [{
+        attachment: `./${scaleName}.mid`,
+        name: `${scaleName}.mid`
+        }]
+      })
+      .then(() => {
+        fs.unlink(`${scaleName}.mid`, (err) => {
+          if(err) console.log(err)
+        });
+      });
+    }
   }
 }
