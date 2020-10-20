9150c6475f7b110b22173372ace21c223c67d4e5
suleiman01x <suleiman01x@users.noreply.github.com>
2020-10-20 20:36:33 +0900

    Make options dynamic

--- commands/scale.js
+++ commands/scale.js
@@ -7,6 +7,16 @@ const MidiWriter = require('midi-writer-js');
 
 const scales = ['major', 'minor', 'ionian', 'dorian', 'phrygian', 'lydian', 'mixolydian', 'aeolian', 'locrian', 'pentatonic', 'chromatic', 'blues', 'harmonicminor', 'melodicminor', 'wholetone']
 const fuseScales = new Fuse(scales, {includeScore: true});
+const options = {
+    mode: {
+      name: '-mode',
+      argNums: 1
+    },
+    midi: {
+      name: '-midi',
+      argNums: 0
+    }
+  }
 
 function addNotes(track, teoriaNotes) {
   for (var note in teoriaNotes){ //converts teoria note to scientific. then adds note event to track
@@ -39,17 +49,29 @@ module.exports = {
       note = note.substring(0, note.length - 1)
     }
 
-    //mode option
-    if (args.includes('-mode')) {
-      const commandMode = fuseScales.search(args[args.indexOf('-mode') + 1])[0].item;
-      if (commandMode === 'pentatonic') { //super jank. changes major/minor pentatonic depending on the notes ending in 'm'
+    // parses the options
+    for (var optNum in options) {
+      let optName = options[optNum]
+      if (args.includes(optName.name)) {
+        let indexNum = args.indexOf(optName.name);
+        optName.enabled = true;
+        //inserts arguments in to optName.args
+        optName.args = args.splice(indexNum, optName.argNums + 1);
+        optName.args.shift();
+      }
+    }
+
+    //mode
+    if (options['mode'].enabled) {
+      const commandMode = fuseScales.search(options['mode'].args[0])[0].item;
+      if (commandMode === 'pentatonic') { //super jank. changes major/minor pentatonic depending on if notes end in 'm'
         mode = mode + commandMode;
       } else {
         mode = commandMode;
       }
     }
 
-    //defines 
+    //defines key and scale mode etc.
     var scaleKeyNote = teoria.note('c');
     try { //makes note and catches errors
       scaleKeyNote = teoria.note(note);
@@ -57,22 +79,26 @@ module.exports = {
       console.log(error);
       return message.reply(`${note}は根音になれません`);
     }
-    
+
     const scale = scaleKeyNote.scale(mode);
     const scaleNotes = scale.notes()
     const scaleName = `${note.toUpperCase()}_${capitalize(mode)}`;
 
-    //midi option
-    if (args.includes('-midi')) {
+
+    //midi
+    if (options['midi'].enabled) {
       var track = new MidiWriter.Track();
-      var scaleMidi = addNotes(track, scaleNotes);
-      var writer = new MidiWriter.Writer(scaleMidi)
-      writer.saveMIDI(scaleName);
-      var hasMidi = true;
+      try {
+        const writer = new MidiWriter.Writer(addNotes(track, scaleNotes))
+        writer.saveMIDI(scaleName);
+      } catch(error) {
+        console.log('WTF HAPPENED');
+        options['midi'].enabled = false;
+      }
     }
 
 
     message.channel.send(`${scaleName}: ${capArray(scale.simple())}`);
-    if (hasMidi) sendAndDelete(message, `${scaleName}.mid`);
+    if (options['midi'].enabled) sendAndDelete(message, `${scaleName}.mid`);
   }
 }
